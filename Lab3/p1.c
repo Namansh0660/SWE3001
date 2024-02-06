cab4894c3988047ec0d0bc5498bfd9d06adab525
Namansh <115533264+Namansh0660@users.noreply.github.com>
2024-02-06 12:45:11 +0530

    Update p1.c

--- Lab3/p1.c
+++ Lab3/p1.c
@@ -25,3 +25,42 @@ int main(int argc, char **argv) {
     }
     return 0;
 }
+
+
+
+
+
+
+
+corrected one
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int common_variable = 0;
+
+void parentProcess() {
+    common_variable = 10;
+    printf("Parent process - common variable value: %d\n", common_variable);
+}
+
+void childProcess() {
+    printf("Child process - common variable value: %d\n", common_variable);
+}
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed\n");
+        return 1;
+    } else if (pid == 0) {
+        // Child process
+        childProcess();
+    } else {
+        // Parent process
+        parentProcess();
+    }
+
+    return 0;
+}
