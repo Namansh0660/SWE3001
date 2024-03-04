37b9684f2c76cbfa9ddc95891cd3b1022d9bbee8
Namansh <115533264+Namansh0660@users.noreply.github.com>
2024-03-04 17:51:07 +0530

    Update thread1.c

--- Lab_4/thread1.c
+++ Lab_4/thread1.c
@@ -1,96 +1,46 @@
 #include <stdio.h>
-
 #include <pthread.h>
-
-
-
 void *check_odd_even(void *arg) {
-
     int num = *((int*)arg);
-
     if (num % 2 == 0) {
-
         printf("Thread ID: %ld - %d is even.\n", pthread_self(), num);
-
     } else {
-
         printf("Thread ID: %ld - %d is odd.\n", pthread_self(), num);
-
     }
-
     pthread_exit(NULL);
-
 }
-
-
 
 void *check_prime(void *arg) {
-
     int num = *((int*)arg);
-
     int flag = 0;
-
     for (int i = 2; i <= num / 2; ++i) {
-
         if (num % i == 0) {
-
             flag = 1;
-
             break;
-
         }
-
     }
-
     if (num <= 1 || flag == 1) {
-
         printf("Thread ID: %ld - %d is not a prime number.\n", pthread_self(), num);
-
     } else {
-
         printf("Thread ID: %ld - %d is a prime number.\n", pthread_self(), num);
-
     }
-
     pthread_exit(NULL);
-
 }
-
-
 
 int main() {
-
     int num;
-
     printf("Enter a number: ");
-
     scanf("%d", &num);
-
-
-
     pthread_t tid1, tid2;
 
-    
-
     // Create first thread to check odd/even
-
     pthread_create(&tid1, NULL, check_odd_even, &num);
-
-    // Create second thread to check prime
-
-    pthread_create(&tid2, NULL, check_prime, &num);
-
     
-
+    // Create second thread to check prime
+    pthread_create(&tid2, NULL, check_prime, &num);
+    
     // Wait for both threads to finish
-
     pthread_join(tid1, NULL);
-
     pthread_join(tid2, NULL);
-
-
-
     return 0;
-
 }
-
