43e6ebd6db8980d78d9c9f86da6d12b6c43d23fb
Namansh <115533264+Namansh0660@users.noreply.github.com>
2024-03-05 12:54:21 +0530

    Update Q2.c

--- Lab_5/Q2.c
+++ Lab_5/Q2.c
@@ -1,7 +1,20 @@
+ /* 2. i. Consider an online delivery app, which delivers the product to the customer
+          who have booked online. The app always chooses the nearest customer first
+          for deliver and then next customer. Suppose 2 customers are booking the product
+          at the same time then the nearest customer will be chosen first.Consider the
+          booking time as arrival time. Choose an appropriate scheduling algorithm to 
+          delivery the product in an efficient manner(bustime = distance)
+
+      ii. For the same app compute for the frequent customer will be given discount 
+          and consider that the customer is chosen with discount code as highest 
+          protity(busttime = distance, priority = frequency)
+
+    Write a program in c to implement 2 scheduling algorithm for case i and ii.
+    Display all results on screen.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-
-// Structure to represent a customer
 typedef struct {
     int id;
     int arrival_time;
@@ -9,13 +22,11 @@ typedef struct {
     int priority; // Priority for case ii
 } Customer;
 
-// Function to compute waiting time for SJF
 void findWaitingTimeSJF(Customer customers[], int n, int waiting_time[]) {
     int service_time[n];
     service_time[0] = 0;
     waiting_time[0] = 0;
 
-    // Compute service time
     for (int i = 1; i < n; i++) {
         service_time[i] = service_time[i - 1] + customers[i - 1].distance;
         waiting_time[i] = service_time[i] - customers[i].arrival_time;
@@ -25,13 +36,11 @@ void findWaitingTimeSJF(Customer customers[], int n, int waiting_time[]) {
     }
 }
 
-// Function to compute waiting time for Priority scheduling
 void findWaitingTimePriority(Customer customers[], int n, int waiting_time[]) {
     int service_time[n];
     service_time[0] = 0;
     waiting_time[0] = 0;
 
-    // Compute service time
     for (int i = 1; i < n; i++) {
         service_time[i] = service_time[i - 1] + customers[i - 1].distance;
         waiting_time[i] = service_time[i] - customers[i].arrival_time;
@@ -41,34 +50,25 @@ void findWaitingTimePriority(Customer customers[], int n, int waiting_time[]) {
     }
 }
 
-// Function to calculate turnaround time
 void findTurnAroundTime(Customer customers[], int n, int waiting_time[], int turnaround_time[]) {
     for (int i = 0; i < n; i++) {
         turnaround_time[i] = customers[i].distance + waiting_time[i];
     }
 }
 
-// Function to compute average time
 void findAvgTime(Customer customers[], int n) {
     int waiting_time[n], turnaround_time[n];
 
-    // Find waiting time of all customers
     findWaitingTimeSJF(customers, n, waiting_time);
-
-    // Find turnaround time for all customers
     findTurnAroundTime(customers, n, waiting_time, turnaround_time);
 
-    // Display results
     printf("Customer ID\t Arrival Time\t Distance\t Waiting Time\t Turnaround Time\n");
-    printf("--------------------------------------------------------------------\n");
     for (int i = 0; i < n; i++) {
         printf("%d\t\t %d\t\t %d\t\t %d\t\t %d\n", customers[i].id, customers[i].arrival_time, 
                customers[i].distance, waiting_time[i], turnaround_time[i]);
     }
 
-    // Display Gantt chart
     printf("\nGantt Chart:\n");
-    printf(" ");
     for (int i = 0; i < n; i++) {
         printf("| Customer %d ", customers[i].id);
     }
@@ -86,14 +86,11 @@ void findAvgTime(Customer customers[], int n) {
 int main() {
     int n;
 
-    // Input number of customers
     printf("Enter the number of customers: ");
     scanf("%d", &n);
 
-    // Allocate memory for customers
     Customer *customers = (Customer*)malloc(n * sizeof(Customer));
 
-    // Input data for each customer
     for (int i = 0; i < n; i++) {
         customers[i].id = i + 1;
         printf("\nEnter details for Customer %d:\n", i + 1);
@@ -106,15 +103,12 @@ int main() {
         scanf("%d", &customers[i].priority);
     }
 
-    // Display results for SJF
     printf("\nResults for SJF (Shortest Job First):\n");
     findAvgTime(customers, n);
 
-    // Display results for Priority scheduling
     printf("\nResults for Priority Scheduling:\n");
     findAvgTime(customers, n);
 
-    // Free allocated memory
     free(customers);
 
     return 0;
