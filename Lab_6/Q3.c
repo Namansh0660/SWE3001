85b065cd8ae7de4114c83f57bcd1ddec8d356b9c
Namansh <115533264+Namansh0660@users.noreply.github.com>
2024-03-26 12:43:28 +0530

    Update Q3.c

--- Lab_6/Q3.c
+++ Lab_6/Q3.c
@@ -1,76 +1,85 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
 
 #define NUM_PHILOSOPHERS 5
+#define NUM_ITERATIONS 10
 
-typedef enum { THINKING, HUNGRY, EATING } state_t;
+typedef enum { THINKING, HUNGRY, EATING } PhilosopherState;
 
-pthread_mutex_t monitor_mutex;
+pthread_mutex_t mutex;
 pthread_cond_t cond_vars[NUM_PHILOSOPHERS];
-state_t philosophers[NUM_PHILOSOPHERS];
+PhilosopherState states[NUM_PHILOSOPHERS];
+int iterations = 0;
 
-void test(int phil_id) {
-    if (philosophers[phil_id] == HUNGRY &&
-        philosophers[(phil_id + 1) % NUM_PHILOSOPHERS] != EATING &&
-        philosophers[(phil_id + 4) % NUM_PHILOSOPHERS] != EATING) {
-        philosophers[phil_id] = EATING;
-        printf("Philosopher %d is eating\n", phil_id);
-        pthread_cond_signal(&cond_vars[phil_id]);
+void monitor_init() {
+    pthread_mutex_init(&mutex, NULL);
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+        pthread_cond_init(&cond_vars[i], NULL);
+        states[i] = THINKING;
     }
 }
 
-void take_forks(int phil_id) {
-    pthread_mutex_lock(&monitor_mutex);
-    philosophers[phil_id] = HUNGRY;
-    printf("Philosopher %d is hungry\n", phil_id);
-    test(phil_id);
-    if (philosophers[phil_id] != EATING) {
-        pthread_cond_wait(&cond_vars[phil_id], &monitor_mutex);
+void monitor_take_forks(int philosopher_id) {
+    pthread_mutex_lock(&mutex);
+    states[philosopher_id] = HUNGRY;
+    while ((states[(philosopher_id + 1) % NUM_PHILOSOPHERS] == EATING) ||
+           (states[(philosopher_id + NUM_PHILOSOPHERS - 1) % NUM_PHILOSOPHERS] == EATING)) {
+        pthread_cond_wait(&cond_vars[philosopher_id], &mutex);
     }
-    pthread_mutex_unlock(&monitor_mutex);
+    states[philosopher_id] = EATING;
+    pthread_mutex_unlock(&mutex);
 }
 
-void put_forks(int phil_id) {
-    pthread_mutex_lock(&monitor_mutex);
-    philosophers[phil_id] = THINKING;
-    printf("Philosopher %d is thinking\n", phil_id);
-    test((phil_id + 1) % NUM_PHILOSOPHERS);
-    test((phil_id + 4) % NUM_PHILOSOPHERS);
-    pthread_mutex_unlock(&monitor_mutex);
+void monitor_put_forks(int philosopher_id) {
+    pthread_mutex_lock(&mutex);
+    states[philosopher_id] = THINKING;
+    pthread_cond_signal(&cond_vars[(philosopher_id + 1) % NUM_PHILOSOPHERS]);
+    pthread_cond_signal(&cond_vars[(philosopher_id + NUM_PHILOSOPHERS - 1) % NUM_PHILOSOPHERS]);
+    pthread_mutex_unlock(&mutex);
 }
 
-void *philosopher(void *arg) {
-    int phil_id = *((int *)arg);
+void* philosopher(void* arg) {
+    int philosopher_id = *((int*)arg);
 
-    while (1) {
-        sleep(rand() % 3 + 1);
-        take_forks(phil_id);
-        sleep(rand() % 3 + 1);
-        put_forks(phil_id);
+    while (iterations < NUM_ITERATIONS) {
+        // Think
+        usleep(rand() % 1000);
+
+        // Pick up forks
+        monitor_take_forks(philosopher_id);
+        printf("Philosopher %d is eating\n", philosopher_id);
+        usleep(rand() % 1000);
+
+        // Put down forks
+        monitor_put_forks(philosopher_id);
+        printf("Philosopher %d is thinking\n", philosopher_id);
+
+        iterations++;
     }
 
     return NULL;
 }
 
 int main() {
-    pthread_t threads[NUM_PHILOSOPHERS];
-    int phil_ids[NUM_PHILOSOPHERS];
+    pthread_t philosophers[NUM_PHILOSOPHERS];
+    int ids[NUM_PHILOSOPHERS];
 
-    pthread_mutex_init(&monitor_mutex, NULL);
+    monitor_init();
+
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        pthread_cond_init(&cond_vars[i], NULL);
-        phil_ids[i] = i;
-        pthread_create(&threads[i], NULL, philosopher, &phil_ids[i]);
+        ids[i] = i;
+        pthread_create(&philosophers[i], NULL, philosopher, &ids[i]);
     }
 
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        pthread_join(threads[i], NULL);
+        pthread_join(philosophers[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
         pthread_cond_destroy(&cond_vars[i]);
     }
-
-    pthread_mutex_destroy(&monitor_mutex);
 
     return 0;
 }
